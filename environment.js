efa33577251d40cb664b62c1572357dbd87381c1
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-07-20 19:43:57 +0530

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
 export const env = {
-    DB_URI: 'mongodb+srv://kowshik-47:Ksefor-47@cluster0.1dh9lqe.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0',
+    DB_URI: '',
     API_URL: 'http://localhost:4000/api',
     url: '',
     PORT: 4000
