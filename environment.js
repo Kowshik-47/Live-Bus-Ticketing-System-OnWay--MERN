a6e55c4929601da3e53fb5a5860cf8ccde77ef0b
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-07-20 19:42:12 +0530

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,6 @@
+export const env = {
+    DB_URI: 'mongodb+srv://kowshik-47:Ksefor-47@cluster0.1dh9lqe.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0',
+    API_URL: 'http://localhost:4000/api',
+    url: '',
+    PORT: 4000
+}
